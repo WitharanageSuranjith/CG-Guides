5862ff906dd1e5af73df9499baff8b732b49c87d
WitharanageSuranjith <witharanagesuranjith@gmail.com>
2025-09-08 21:15:29 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "CG Guide",
+  title: "CG Guides",
   description: "Online Courses Platform in Art, Games & VFX",
 };
 
